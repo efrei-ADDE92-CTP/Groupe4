f644899e5a45638632d86d9856a39a520cf0124e
Christopher2023 <66922510+Christopher2023@users.noreply.github.com>
2023-01-24 17:48:59 +0100

    Prometheus_done

--- API/API.py
+++ API/API.py
@@ -1,8 +1,13 @@
-from flask import Flask,request
+from flask import Response, Flask,request
 import joblib
 import json
+from prometheus_client import Counter
+from prometheus_client.core import CollectorRegistry
+import prometheus_client
 
 app = Flask(__name__)
+
+c = Counter("Classifier_g4_request_count", "Number of requests processed")
 
 @app.before_first_request
 def load_model():
@@ -12,6 +17,7 @@ def load_model():
 
 @app.route('/predict', methods=['POST'])
 def predictions():
+    c.inc()
     data = json.loads(request.data)
     sepal_length = data["sepal length"]
     sepal_width = data["sepal width"]
@@ -20,5 +26,11 @@ def predictions():
     prediction = model.predict([[sepal_length, sepal_width,petal_length,petal_width]])
     labels = ['setosa', 'versicolor', 'virginica']
     return {"prediction" : str(labels[prediction[0]])}
+
+@app.route("/metrics")
+def metrics():
+    res = []
+    res.append(prometheus_client.generate_latest(c))
+    return Response(res, mimetype="text/plain")
 
 app.run("0.0.0.0",port = 80, debug = True)
